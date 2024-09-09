af82089d6b9c8892d42a5ae07ff615c88afdbf44
RafaelSpumberg <150760513+RafaelSpumberg@users.noreply.github.com>
2024-09-09 16:20:00 +0000

    Alterações meio dia

--- Q2_LogSimples/src/App.java
+++ Q2_LogSimples/src/App.java
@@ -1,11 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        LogSimples log = new LogSimples();
+        LogSimples log = LogSimples.getInstancia();
+        
         log.log("Mensagem de alerta 1");
         log.log("Mensagem de alarme 23");
         log.log("Mensagem de PANICO!!");
 
-        for(String m:log){
+        // Itera sobre as mensagens e as imprime
+        for (String m : log) {
             System.out.println(m);
         }
     }
--- Q2_LogSimples/src/LogSimples.java
+++ Q2_LogSimples/src/LogSimples.java
@@ -3,14 +3,21 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class LogSimples implements Iterable<String>{
+public class LogSimples implements Iterable<String> {
+    private static LogSimples instanciaUnica;
     private List<String> mensagens;
-
-    public LogSimples(){
+    private LogSimples() {
         mensagens = new LinkedList<>();
     }
 
-    public void log(String m){
+    public static LogSimples getInstancia() {
+        if (instanciaUnica == null) {
+            instanciaUnica = new LogSimples();
+        }
+        return instanciaUnica;
+    }
+
+    public void log(String m) {
         String logM = LocalDate.now().toString() + " : " + m;
         mensagens.add(logM);
     }
